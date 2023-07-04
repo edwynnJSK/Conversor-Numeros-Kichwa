ff4f1a52dbb0c12444a51d772ec2200295b0a18d
edwynnJSK <111151639+edwynnJSK@users.noreply.github.com>
2023-07-04 15:49:44 -0500

    Add files via upload

---
+++ Código/Kichwa-main/src/main/java/Kichwa.java
@@ -0,0 +1,32 @@
+
+public class Kichwa {
+
+    private NumeroKichwa[] numerosKichwa;
+
+    public Kichwa() {
+        this.numerosKichwa = new NumeroKichwa[20];
+    }
+
+    public void agregarNumero(NumeroKichwa... numerosKichwa) {
+        this.numerosKichwa = numerosKichwa;
+    }
+
+    public String buscarNumero(int numero) {
+        String resultado = "";
+        for (NumeroKichwa numeroKichwa : numerosKichwa) {
+            if (numero == numeroKichwa.getValorNumerico() && numeroKichwa != null) {
+                resultado = numeroKichwa.getValorVerbal();
+            }
+        }
+        return resultado;
+    }
+
+    public boolean estaEnLista(int numeroABuscar) {
+        for (NumeroKichwa numeroKichwa : numerosKichwa) {
+            if (numeroKichwa != null && numeroKichwa.getValorNumerico() == numeroABuscar) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
